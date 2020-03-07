cc1995a2bced3729f5fa23353e7a11f211407893
ismail <ismailelmaslouhy@gmail.com>
2020-03-07 01:06:31 +0100

    trying axios

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
@@ -13,21 +15,35 @@ export const createProduct = (title, imageUrl, description, price) => {
   return async dispatch => {
     //any async code You want , thanks for Thunk middelware
 
-    const response = await fetch(
-      "https://rn-shop-b6d42.firebaseio.com/products.json",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          title,
-          imageUrl,
-          description,
-          price
-        })
-      }
-    );
+    // const response = await fetch(
+    //   "https://rn-shop-b6d42.firebaseio.com/products.json",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //       title,
+    //       imageUrl,
+    //       description,
+    //       price
+    //     })
+    //   }
+    // );
+
+    axios
+      .post("https://rn-shop-b6d42.firebaseio.com/products.json", {
+        title,
+        imageUrl,
+        description,
+        price
+      })
+      .then(resp => {
+        console.log(resp);
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
     const resData = await response.json();
     console.log(resData);
